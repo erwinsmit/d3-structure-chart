7b00d846f9b1714a99a30835a20ce30b56e6b332
erwinsmit <erwin.smit@gmail.com>
2019-01-31 15:06:12 +0100

    also added bottom bit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const svg = d3.select("svg");
 
 const sizes = {
   svgContainer: {
-    width: 800,
-    height: 800
+    width: 700,
+    height: 700,
+    margin: 50
   },
   rectangle: {
     width: 150,
@@ -30,19 +31,41 @@ const data = {
     },
     {
       title: "Company Secretary",
-      subtitle: "Company a"
+      subtitle: "Important company"
     },
     {
       title: "Director",
       subtitle: "John Jones"
     }
+  ],
+  bottom: [
+    {
+      title: "John JONES",
+      subtitle: "Director"
+    },
+    {
+      title: "KPMG, Luxembourg",
+      subtitle: "Auditor"
+    },
+    {
+      title: "Orca, Tokyo",
+      subtitle: "Shareholder"
+    },
+    {
+      title: "ABN Amro, London",
+      subtitle: "Bank"
+    }
   ]
 };
 
 const topRectangleBinding = svg.selectAll("rect").data(data.top);
+const bottomRectangleBinding = svg.selectAll("rect").data(data.bottom);
 
 const topAreas = topRectangleBinding.enter().append("g");
 const topRectangles = topAreas.append("rect");
+
+const bottomAreas = bottomRectangleBinding.enter().append("g");
+const bottomRectangles = bottomAreas.append("rect");
 
 function setRectangles(rectangles) {
   rectangles
@@ -73,33 +96,39 @@ function setLabelsToAreas(areas) {
     .attr("y", "50");
 }
 
-function setAreas(areas) {
+function setAreas(areas, position) {
   const areaWidthWithMargin = sizes.rectangle.width + sizes.rectangle.margin;
   const areaCount = areas._groups[0].length;
   const totalAreasWidth = areaCount * areaWidthWithMargin;
-  const offset = (sizes.svgContainer.width - totalAreasWidth) / 2;
-  const getXValue = i => i * areaWidthWithMargin + offset;
 
-  console.log("areas", areas, areaCount, totalAreasWidth, offset);
+  const offset = (sizes.svgContainer.width - totalAreasWidth) / 2 + sizes.rectangle.margin / 2;
+  const getXValue = i => i * areaWidthWithMargin + offset;
+  const yPosition = position === "top" ? sizes.svgContainer.margin : sizes.svgContainer.height - sizes.rectangle.height - sizes.svgContainer.margin;
 
   areas
     .attr("transform", (d, i) => {
-      return `translate(${getXValue(i)}, 50)`;
+      return `translate(${getXValue(i)}, ${yPosition})`;
     })
+    .attr("position", position)
     .attr("width", sizes.rectangle.width)
     .attr("height", sizes.rectangle.height)
     .attr("x", (d, i) => getXValue(i))
-    .attr("y", 50);
+    .attr("y", yPosition);
 }
 
 setRectangles(topRectangles);
+setRectangles(bottomRectangles);
+
 setLabelsToAreas(topAreas);
-setAreas(topAreas);
+setLabelsToAreas(bottomAreas);
+
+setAreas(topAreas, "top");
+setAreas(bottomAreas, "bottom");
 
 const mainCircle = svg
   .append("circle")
-  .attr("cx", sizes.svgContainer.width / 2 - sizes.centreCircle.width / 2)
-  .attr("cy", sizes.svgContainer.height / 2 - sizes.centreCircle.width / 2)
+  .attr("cx", sizes.svgContainer.width / 2)
+  .attr("cy", sizes.svgContainer.height / 2)
   .attr("r", sizes.centreCircle.width / 2)
   .style("fill", "#114B5F");
 
@@ -109,13 +138,18 @@ const lineCreater = d3
   .y(d => d.y)
   .curve(d3.curveBasis);
 
-const drawLines = function(objects) {
+const drawLines = function(objects, position) {
   var lines = [];
 
   const mainCircleElement = mainCircle._groups[0][0];
   const objectElements = objects._groups[0];
 
   lines = objectElements.map(objectElement => {
+    const lineOffsets = {
+      mainCircleOffset: position === "top" ? mainCircleElement.getAttribute("cy") - 80 : parseInt(mainCircleElement.getAttribute("cy")) + 80,
+      objectOffset: position === "top" ? parseInt(objectElement.getAttribute("y")) + 150 : parseInt(objectElement.getAttribute("y")) - 55
+    };
+
     return [
       {
         x: mainCircleElement.getAttribute("cx"),
@@ -123,11 +157,11 @@ const drawLines = function(objects) {
       },
       {
         x: mainCircleElement.getAttribute("cx"),
-        y: mainCircleElement.getAttribute("cy") - 80
+        y: lineOffsets.mainCircleOffset
       },
       {
         x: parseInt(objectElement.getAttribute("x")) + sizes.rectangle.width / 2,
-        y: parseInt(objectElement.getAttribute("y")) + 150
+        y: lineOffsets.objectOffset
       },
       {
         x: parseInt(objectElement.getAttribute("x")) + sizes.rectangle.width / 2,
@@ -152,4 +186,5 @@ const drawLines = function(objects) {
   });
 };
 
-drawLines(topAreas);
+drawLines(topAreas, "top");
+drawLines(bottomAreas, "bottom");
